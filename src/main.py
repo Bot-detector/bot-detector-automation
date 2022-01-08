49c117acdb03839b7804b659ebea63e053001bd9
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2022-01-08 17:48:25 -0500

    Set our two current tasks as apscheduler jobs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
 import tipoff.main_tipoff
 import banbroadcaster.main_broadcaster
 
+scheduler = BackgroundScheduler()
+
 if __name__ == '__main__':
-    banbroadcaster.main_broadcaster.broadcast_bans()
-    tipoff.main_tipoff.tipoff_bots()
+    #Broadcast bans to #bot-graveyard on Discord, sent total bans tweet, and send bans breakdowns tweets
+    scheduler.add_job(banbroadcaster.main_broadcaster.broadcast_bans, 'cron', hour=20, minute=5, misfire_grace_time=60)
+
+    #Send next tipoff batch to Jagex
+    scheduler.add_job(tipoff.main_tipoff.tipoff_bots, 'cron', hour=20, minute=45, misfire_grace_time=60)
+
+    scheduler.start()
