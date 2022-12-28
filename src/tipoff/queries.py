55d70f0642e002d92d527e3f5388a82700d1ec99
extreme4all <40169115+extreme4all@users.noreply.github.com>
2022-12-29 00:57:05 +0100

    Update queries.py

--- src/tipoff/queries.py
+++ src/tipoff/queries.py
@@ -1,22 +1,23 @@
 TIPOFF_CONFIG = '''
-                SELECT DISTINCT
-                    pl.name,
-                    UNIX_TIMESTAMP(rp.timestamp)*1000 as timestamp,
-                    rid.region_name,
-                    p.prediction,
-                    p.Predicted_confidence,
-                    phd.*,
-                    rp.*
-                FROM Players as pl
-                INNER JOIN Predictions as p on (p.id = pl.id)
-                INNER JOIN reportLatest as rp on (rp.reported_id = pl.id)
-                INNER JOIN playerHiscoreDataLatest as phd on phd.Player_id = pl.id
-                INNER JOIN regionIDNames as rid on rid.region_ID = rp.region_ID
-                WHERE 1=1
-                    and p.Predicted_confidence > 99
-                    and pl.possible_ban = 0
-                    and pl.confirmed_ban = 0
-                    and p.prediction != 'Real_Player'
-                    and p.prediction != 'Stats Too Low'
-                    and rp.timestamp >= current_timestamp() - interval 1 day
-                '''
+    SELECT DISTINCT
+        pl.name,
+        UNIX_TIMESTAMP(rp.timestamp)*1000 as timestamp,
+        rid.region_name,
+        p.prediction,
+        p.Predicted_confidence
+    FROM Players as pl
+    INNER JOIN Predictions as p on (p.id = pl.id)
+    INNER JOIN reportLatest as rp on (rp.reported_id = pl.id)
+    INNER JOIN playerHiscoreDataLatest as phd on phd.Player_id = pl.id
+    INNER JOIN regionIDNames as rid on rid.region_ID = rp.region_ID
+    WHERE 1=1
+        and p.Predicted_confidence > 99
+        and pl.label_jagex = 0
+        and p.prediction not in (
+            'Real_Player',
+            'Stats Too Low',
+            'Stats_Too_Low'
+        )
+        and rp.timestamp >= current_timestamp() - interval 1 day
+    order by p.prediction desc
+'''
