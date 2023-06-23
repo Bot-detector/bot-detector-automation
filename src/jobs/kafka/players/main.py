2150886197ef3eb15e5d8aa4bb2ed8a47cc71625
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-06-23 18:56:10 +0200

    cleaner code

--- src/jobs/kafka/players/main.py
+++ src/jobs/kafka/players/main.py
@@ -13,6 +13,7 @@ from . import queries
 import logging
 import sys
 from sqlalchemy.exc import OperationalError
+from sqlalchemy import Engine
 
 logger = logging.getLogger(__name__)
 APPCONFIG = config.AppConfig()
@@ -50,80 +51,84 @@ async def send_rows_to_kafka(rows: list[Player], kafka_topic: str):
         await producer.stop()
 
 
-async def async_main(unique_ids: list = []):
+def get_data(query: str, engine: Engine):
+    with engine.connect() as connection:
+        # Create a SQLAlchemy text object from the batch SQL query
+        statement = sqlalchemy.text(query)
+
+        # Execute the batch SQL query
+        result = connection.execute(statement)
+
+        # Fetch the column names from the result
+        columns = result.keys()
+        result = result.fetchall()
+    return columns, result
+
+
+def process_rows(result, unique_ids: list, columns: list):
+    today = datetime.now().strftime("%Y-%m-%d")
+    # Iterate over the result set and convert rows to dictionaries
+    rows = []
+    for row in result:
+        row = dict(zip(columns, row))
+
+        if row["created_at"]:
+            row["created_at"] = row["created_at"].strftime("%Y-%m-%d %H:%M:%S")
+
+        if row["updated_at"]:
+            if row["updated_at"].strftime("%Y-%m-%d") == today:
+                continue
+            row["updated_at"] = row["updated_at"].strftime("%Y-%m-%d %H:%M:%S")
+
+        row = Player(**row)
+
+        if row.id not in unique_ids:
+            rows.append(row)
+            # Add unique IDs to the list
+            unique_ids.append(row.id)
+    return rows, unique_ids
+
+
+async def async_main():
     APPCONFIG = config.AppConfig()
     connection_string = f"mysql+pymysql://{APPCONFIG.SERVER_LOGIN}:{APPCONFIG.SERVER_PASSWORD}@{APPCONFIG.SERVER_ADDRESS}/{APPCONFIG.DATABASE}"
 
     # Execute the SQL query in batches of size 10,000
     batch_size: int = 5000
     offset: int = 0
-    today = datetime.now().strftime("%Y-%m-%d")
+    unique_ids: list = []
 
-    # indent hell
+    logger.info("Creating database engine")
+
+    # Create an engine and establish the connection
+    engine = sqlalchemy.create_engine(connection_string)
+
     while True:
+        # Construct the SQL query with the batch size and offset
+        query: str = f"{queries.sql} LIMIT {batch_size} OFFSET {offset};"
         try:
-            logger.info("Creating database engine")
-            # Create an engine and establish the connection
-            engine = sqlalchemy.create_engine(connection_string)
-            with engine.connect() as connection:
-                while True:
-                    # Construct the SQL query with the batch size and offset
-                    batch_sql: str = (
-                        f"{queries.sql} LIMIT {batch_size} OFFSET {offset};"
-                    )
-
-                    # Create a SQLAlchemy text object from the batch SQL query
-                    statement = sqlalchemy.text(batch_sql)
-
-                    # Execute the batch SQL query
-                    result = connection.execute(statement)
-
-                    # Fetch the column names from the result
-                    column_names = result.keys()
-                    result = result.fetchall()
-
-                    # Iterate over the result set and convert rows to dictionaries
-                    rows = []
-                    for row in result:
-                        row = dict(zip(column_names, row))
-
-                        if row["created_at"]:
-                            row["created_at"] = row["created_at"].strftime(
-                                "%Y-%m-%d %H:%M:%S"
-                            )
-
-                        if row["updated_at"]:
-                            if row["updated_at"].strftime("%Y-%m-%d") == today:
-                                continue
-                            row["updated_at"] = row["updated_at"].strftime(
-                                "%Y-%m-%d %H:%M:%S"
-                            )
-
-                        row = Player(**row)
-
-                        if row.id not in unique_ids:
-                            rows.append(row)
-                            # Add unique IDs to the list
-                            unique_ids.append(row.id)
-
-                    if not rows:
-                        continue
-
-                    logger.debug(f"{len(unique_ids)=}, {offset=}")
-
-                    # Send rows to Kafka
-                    await send_rows_to_kafka(rows, kafka_topic="player")
-
-                    # Increment the offset for the next batch
-                    offset += batch_size
-
-        except OperationalError as e:
+            columns, result = get_data(query, engine)
+        except Exception as e:
             logger.error(f"exception {str(e)}")
-
-            # reset offset
             offset = 0
             await asyncio.sleep(60)
-            
+            continue
+
+        rows, unique_ids = process_rows(result, unique_ids, columns)
+
+        if not rows:
+            logger.error(f"no rows")
+            offset = 0
+            await asyncio.sleep(60)
+            continue
+
+        logger.debug(f"{len(unique_ids)=}, {offset=}")
+
+        # Send rows to Kafka
+        await send_rows_to_kafka(rows, kafka_topic="player")
+
+        # Increment the offset for the next batch
+        offset += batch_size
 
 
 def get_players_to_scrape():
