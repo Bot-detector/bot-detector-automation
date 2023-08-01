55b54a652dafe1d958a41954b47dcd76ef627140
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-08-02 01:08:38 +0200

    super charge

--- src/jobs/kafka/players/main.py
+++ src/jobs/kafka/players/main.py
@@ -10,89 +10,14 @@ from .models import Player
 import config
 from config import config
 from collections import deque
+from asyncio import Queue
+from aiohttp import ClientResponse
 
 logger = logging.getLogger(__name__)
 APPCONFIG = config.AppConfig()
 
 
-async def send_rows_to_kafka(
-    producer: AIOKafkaProducer, rows: list[Player], kafka_topic: str
-):
-    try:
-        # Send rows to Kafka
-        for row in rows:
-            await producer.send(kafka_topic, key=row.name.encode(), value=row.dict())
-        logger.info(f"send {len(rows)} players to kafka")
-    except Exception as e:
-        logger.error(e)
-        # Stop the Kafka producer
-        await producer.stop()
-    finally:
-        # Wait for all messages to be sent
-        await producer.flush()
-
-
-async def get_data(page: int) -> tuple[list[Player], int]:
-    """
-    This method is used to get the players to scrape from the api.
-    """
-    url = (
-        f"{APPCONFIG.ENDPOINT}/v2/players?page={page}&page_size={APPCONFIG.BATCH_SIZE}"
-        # f"{APPCONFIG.ENDPOINT}/v1/scraper/players/0/{APPCONFIG.BATCH_SIZE}/{APPCONFIG.API_TOKEN}"
-    )
-    headers = {"token": APPCONFIG.API_TOKEN}
-    logger.info("fetching players to scrape")
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as response:
-            if response.status != 200:
-                logger.error(
-                    f"response status {response.status}"
-                    f"response body: {await response.text()}"
-                )
-                raise Exception("error fetching players")
-            players = await response.json()
-    logger.info(f"fetched {len(players)} players")
-    players = [Player(**player) for player in players]
-    return players, page + 1
-
-
-def process_rows(result: list[Player], unique_ids: list):
-    today = datetime.now().strftime("%Y-%m-%d")
-
-    rows = []
-    for row in result:
-        if row.created_at:
-            row.created_at
-
-        if row["created_at"]:
-            row["created_at"] = row["created_at"].strftime("%Y-%m-%dT%H:%M:%S")
-
-        if row["updated_at"]:
-            if row["updated_at"].strftime("%Y-%m-%d") == today:
-                continue
-            row["updated_at"] = row["updated_at"].strftime("%Y-%m-%dT%H:%M:%S")
-
-        row = Player(**row)
-
-        if row.id not in unique_ids:
-            rows.append(row)
-            # Add unique IDs to the list
-            unique_ids.append(row.id)
-    return rows, unique_ids
-
-
-def batch_list(input_list, batch_size):
-    for i in range(0, len(input_list), batch_size):
-        yield input_list[i : i + batch_size]
-
-
-async def async_main():
-    unique_ids = deque(maxlen=300_000)
-
-    logger.info("start getting data")
-    last_day = datetime.now().date()
-    page = 1
-    # Create Kafka producer
+async def send_rows_to_kafka(kafka_topic: str, message_queue: Queue):
     producer = aiokafka.AIOKafkaProducer(
         bootstrap_servers=[APPCONFIG.KAFKA_HOST],
         value_serializer=lambda x: json.dumps(x).encode(),
@@ -100,55 +25,97 @@ async def async_main():
 
     await producer.start()
 
+    try:
+        while True:
+            message: Player = await message_queue.get()
+            await producer.send(
+                kafka_topic, key=message.name.encode(), value=message.dict()
+            )
+            message_queue.task_done()
+            
+            qsize = message_queue.qsize()
+            if qsize % 1000 == 0:
+                logger.info(f"{qsize=}")
+    except Exception as e:
+        logger.error(e)
+    finally:
+        await producer.flush()
+        await producer.stop()
+
+
+async def handle_aiohttp_error(response: ClientResponse):
+    if response.status != 200:
+        logger.error(
+            f"response status {response.status}"
+            f"response body: {await response.text()}"
+        )
+        raise Exception("error fetching players")
+
+
+async def add_data_to_queue(players:list[dict], message_queue: Queue, unique_ids:list[int]):
+    today = datetime.now().date()
+    for player in players:
+        player = Player(**player)
+        date = datetime.strptime(player.updated_at, "%Y-%m-%dT%H:%M:%S").date()
+
+        if date == today:
+            continue
+
+        if player.id in unique_ids:
+            continue
+
+        await message_queue.put(player)
+        unique_ids.append(player.id)
+
+        qsize = message_queue.qsize()
+        if qsize % 1000 == 0:
+            logger.info(f"{qsize=} {len(unique_ids)=}")
+    return 
+
+async def get_data(message_queue: Queue):
+    page = 1
+    unique_ids = deque(maxlen=100_000)
+    headers = {"token": APPCONFIG.API_TOKEN}
+    last_day = datetime.now().date()
+
     while True:
-        # reset on new day
+        if message_queue.qsize() > int(message_queue.maxsize / 2):
+            await asyncio.sleep(1)
+            continue
+
+        url = f"{APPCONFIG.ENDPOINT}/v2/players?page={page}&page_size={APPCONFIG.BATCH_SIZE}"
+        # url = f"{APPCONFIG.ENDPOINT}/v1/scraper/players/0/{APPCONFIG.BATCH_SIZE}/{APPCONFIG.API_TOKEN}"
+        logger.info(f"fetching players to scrape {page=}")
+
         today = datetime.now().date()
+
         if today != last_day:
             last_day = datetime.now().date()
-            unique_ids = []
+            unique_ids.clear()
             page = 1
 
-        # get data
-        result, page = await get_data(page=page)
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.get(url) as response:
+                await handle_aiohttp_error(response)
+                players = await response.json()
+
+        logger.info(f"fetched {len(players)} players")
+
+        if len(players) < APPCONFIG.BATCH_SIZE:
+            logger.info(f"Received {len(players)} at {page} resetting page")
+            page = 1
+            continue
+
         
-        if len(result) < APPCONFIG.BATCH_SIZE:
-            logger.info(f"Received {len(batch)} at {page} resetting page")
-            page = 1
+        asyncio.ensure_future(add_data_to_queue(players, message_queue, unique_ids))
 
-        if not result:
-            logger.info("result is empty")
-            await asyncio.sleep(60)
-            continue
+        page += 1
 
-        rows = []
 
-        for row in result:
-            # check for duplicate id's
-            if row.id in unique_ids:
-                continue
-
-            # check if already scraped for some reason
-            if row.updated_at is not None:
-                _updated_at = datetime.strptime(
-                    row.updated_at, "%Y-%m-%dT%H:%M:%S"
-                ).date()
-                if _updated_at == today:
-                    continue
-
-            unique_ids.append(row.id)
-            rows.append(row)
-
-        # check if there are any rows
-        if not rows:
-            logger.error(f"no unique rows\nexample={row.dict()}")
-            await asyncio.sleep(5)
-            continue
-
-        # Send rows to Kafka
-        for batch in batch_list(rows.copy(), batch_size=1000):
-            asyncio.ensure_future(
-                send_rows_to_kafka(producer, batch, kafka_topic="player")
-            )
+async def async_main():
+    message_queue = Queue(maxsize=10_000)
+    asyncio.ensure_future(get_data(message_queue))
+    asyncio.ensure_future(send_rows_to_kafka("player", message_queue))
 
 
 def get_players_to_scrape():
