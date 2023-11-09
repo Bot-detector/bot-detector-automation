c7812e334d8eb1ef98fa8f20f4e167e5df67ab75
extreme4all <>
2023-11-09 22:45:14 +0100

    rewrite

--- src/jobs/kafka/players/main.py
+++ src/jobs/kafka/players/main.py
@@ -1,175 +1,150 @@
 import asyncio
 import json
 import logging
-from datetime import datetime, timedelta
+from asyncio import Queue
+from datetime import datetime
+from time import time
+from typing import Any
 
 import aiohttp
-import aiokafka
 from aiokafka import AIOKafkaProducer
-from aiohttp import ClientResponse, ClientConnectorError
-from asyncio import Queue
-from collections import deque
-from typing import List
+
+from config import config
 
 from .models import Player
-from config import config
 
 logger = logging.getLogger(__name__)
 APPCONFIG = config.AppConfig()
 
 
-class KafkaProducer:
-    def __init__(self, kafka_topic: str, message_queue: Queue):
-        self.kafka_topic = kafka_topic
-        self.message_queue = message_queue
+async def kafka_producer():
+    producer = AIOKafkaProducer(
+        bootstrap_servers=[APPCONFIG.KAFKA_HOST],
+        value_serializer=lambda v: json.dumps(v).encode(),
+    )
+    await producer.start()
+    return producer
 
-    async def send_messages(self, producer):
-        while True:
-            message: Player = await self.message_queue.get()
-            await producer.send(
-                self.kafka_topic, key=message.name.encode(), value=message.dict()
+
+async def send_messages(topic: str, producer: AIOKafkaProducer, send_queue: Queue):
+    last_interval = time()
+    messages_sent = 0
+
+    while True:
+        if send_queue.empty():
+            await asyncio.sleep(1)
+        message = await send_queue.get()
+        await producer.send(topic, value=message)
+        send_queue.task_done()
+
+        messages_sent += 1
+
+        if messages_sent >= 100:
+            current_time = time()
+            elapsed_time = current_time - last_interval
+            speed = messages_sent / elapsed_time
+            logger.info(
+                f"processed {messages_sent} in {elapsed_time:.2f} seconds, {speed:.2f} msg/sec"
             )
-            self.message_queue.task_done()
 
-            qsize = self.message_queue.qsize()
-            if qsize % 1000 == 0:
-                logger.info(f"{qsize=}")
-
-    async def start(self):
-        logger.info(f"starting: {self.__class__.__name__}")
-        producer = AIOKafkaProducer(
-            bootstrap_servers=[APPCONFIG.KAFKA_HOST],
-            value_serializer=lambda x: json.dumps(x).encode(),
-        )
-        await producer.start()
-
-        try:
-            await self.send_messages(producer=producer)
-        except Exception as e:
-            logger.error(e)
-        finally:
-            await producer.flush()
-            await producer.stop()
-
-        logger.info("restarting")
-        # if for some reason we break the loop, just restart
-        await asyncio.sleep(60)
-        await self.start()
+            last_interval = time()
+            messages_sent = 0
 
 
-class DataFetcher:
-    def __init__(self, message_queue: Queue):
-        self.message_queue = message_queue
-        self.headers = {"token": APPCONFIG.API_TOKEN}
-        self.session = aiohttp.ClientSession(headers=self.headers)
+def is_today(updated_at: str):
+    if updated_at is None:
+        return False
+    today = datetime.now().date()
+    date = datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%S").date()
+    return date == today
 
-    async def add_data_to_queue(self, players: List[dict], unique_ids: List[int]):
-        today = datetime.now().date()
-        added = 0
-        for player in players:
-            player = Player(**player)
 
-            if len(player.name) > 13:
-                continue
+async def parse_data(players: list[dict]):
+    players: list[Player] = [Player(**player) for player in players]
+    players = [
+        player
+        for player in players
+        if len(player.name) < 13 and is_today(player.updated_at)
+    ]
+    return players
 
-            if player.updated_at is not None:
-                date = datetime.strptime(player.updated_at, "%Y-%m-%dT%H:%M:%S").date()
-                if date == today:
-                    continue
 
-            if player.id in unique_ids:
-                continue
-
-            await self.message_queue.put(player)
-            unique_ids.append(player.id)
-            added += 1
-
-            qsize = self.message_queue.qsize()
-            if qsize % 1000 == 0:
-                logger.info(f"{qsize=}, {len(unique_ids)=}")
-        logger.info(f"{added=}")
-
-    async def get_data(self, url: str, params: dict) -> list[dict]:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, headers=self.headers) as resp:
-                if not resp.ok:
-                    error_message = (
-                        f"response status {resp.status} "
-                        f"response body: {await resp.text()}"
-                    )
-                    logger.error(error_message)
-                    raise ValueError(error_message)
+async def get_request(
+    url: str, params: dict, headers: dict = {}
+) -> tuple(list[dict], Any):
+    data = None
+    error = None
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, headers=headers) as resp:
+            if resp.ok:
                 data = await resp.json()
-                return data
+            else:
+                error = {
+                    "status": resp.status,
+                    "body": await resp.text(),
+                    "url": url,
+                    "params": params,
+                }
+                logger.error(error)
+    return data, error
 
-    async def main(self):
-        logger.info(f"starting: {self.__class__.__name__}")
 
-        unique_ids = deque(maxlen=500_000)
-        last_day = datetime.now().date()
-        max_id = 0
+async def get_data(receive_queue: Queue):
+    last_day = datetime.now().date()
+    max_id = 0
+    params = {
+        "limit": APPCONFIG.BATCH_SIZE,
+        "player_id": max_id,
+        "greater_than": 1,
+    }
+    headers = {"token": APPCONFIG.API_TOKEN}
+    url = f"{APPCONFIG.ENDPOINT}/v2/player/"
 
-        params = {
-            "limit": APPCONFIG.BATCH_SIZE,
-            "player_id": max_id,
-            "greater_than": 1,
-        }
-        url = f"{APPCONFIG.ENDPOINT}/v2/player/"
-        while True:
-            if self.message_queue.qsize() > int(self.message_queue.maxsize / 2):
-                await asyncio.sleep(1)
-                continue
+    while True:
+        today = datetime.now().date()
+        players, error = await get_request(url=url, params=params, headers=headers)
 
-            today = datetime.now().date()
+        if error is not None:
+            sleep_time = 30
+            logger.info(f"sleeping {sleep_time}")
+            await asyncio.sleep(sleep_time)
+            continue
 
-            if today != last_day:
-                logger.info("new day, resetting")
-                last_day = datetime.now().date()
-                unique_ids.clear()
-                params["player_id"] = 0
+        players = await parse_data(players=players)
 
-            logger.info(f"fetching players to scrape {max_id=}")
+        await asyncio.gather(*[receive_queue.put(item=p) for p in players])
 
-            try:
-                players = await self.get_data(url=url, params=params)
-            except Exception as error:
-                logger.error(f"An error occurred: {type(error)} - {str(error)}")
-                await asyncio.sleep(5)
-                continue
+        if len(players) < APPCONFIG.BATCH_SIZE:
+            logger.info(f"Received {len(players)}, sleeping.")
+            await asyncio.sleep(300)
 
-            logger.info(f"fetched {len(players)} players")
+        max_id = max([p.id for p in players])
 
-            if len(players) < APPCONFIG.BATCH_SIZE:
-                logger.info(f"Received {len(players)}")
-                await asyncio.sleep(300)
-
-            asyncio.ensure_future(self.add_data_to_queue(players, unique_ids))
-
-            players_max = max([p.get("id") for p in players])
-            max_id = players_max if players_max > max_id else max_id
+        if max_id > params["player_id"]:
             params["player_id"] = max_id
 
+        if today == last_day:
+            params["player_id"] = 0
 
-async def retry(f) -> None:
-    class_name = f.__self__.__class__.__name__
-    method_name = f.__name__
+
+async def main():
+    send_queue = Queue()
+    receive_queue = Queue()
+    producer = await kafka_producer()
+
+    asyncio.create_task(get_data(receive_queue=receive_queue))
+    asyncio.create_task(
+        send_messages(topic="player", producer=producer, send_queue=send_queue)
+    )
+
     while True:
-        try:
-            logger.info(f"starting: {class_name}.{method_name}")
-            await f()
-        except Exception as e:
-            logger.error(f"{class_name}.{method_name} - {e}")
-            await asyncio.sleep(15)
+        if receive_queue.empty():
+            await asyncio.sleep(1)
 
-
-async def async_main():
-    message_queue = Queue(maxsize=10_000)
-    data_fetcher = DataFetcher(message_queue)
-    kafka_producer = KafkaProducer("player", message_queue)
-
-    asyncio.ensure_future(data_fetcher.main())
-    asyncio.ensure_future(kafka_producer.start())
+        message = await receive_queue.get()
+        await send_queue.put(message)
+        receive_queue.task_done()
 
 
 def get_players_to_scrape():
-    asyncio.ensure_future(async_main())
+    asyncio.ensure_future(main())
