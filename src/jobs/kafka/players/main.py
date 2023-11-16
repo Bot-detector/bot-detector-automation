c7fdd34b8f1caa9c84263e963333b35d2d3faad3
extreme4all <>
2023-11-16 19:52:46 +0100

    reset today = last_day

--- src/jobs/kafka/players/main.py
+++ src/jobs/kafka/players/main.py
@@ -130,6 +130,7 @@ async def get_data(receive_queue: Queue):
         if today != last_day:
             logger.info("New day!, resetting player_id to 0")
             params["player_id"] = 0
+            today = last_day
 
         if len_players < APPCONFIG.BATCH_SIZE:
             sleep_time = 300
