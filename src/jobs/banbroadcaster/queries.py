f8a051f8ce317b17163165ad79ec0486951fd3e0
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-02-12 00:10:08 +0100

    bug fix

--- src/jobs/banbroadcaster/queries.py
+++ src/jobs/banbroadcaster/queries.py
@@ -2,9 +2,11 @@ sql_get_count_banned_players = """
 SELECT
 COUNT(*) as bans
 FROM Players
-WHERE (possible_ban = 1
-AND confirmed_ban = 0
-AND (label_jagex = 2 or label_jagex = 3))
+WHERE 1=1
+    AND possible_ban = 1
+    AND confirmed_ban = 0
+    AND label_jagex = 2
+)
 """
 
 sql_get_count_banned_real_players = """
@@ -14,7 +16,7 @@ sql_get_count_banned_real_players = """
     JOIN Predictions pred on pred.name = pls.name
     WHERE possible_ban = 1 
         AND confirmed_ban = 0
-        AND (label_jagex = 2 or label_jagex = 3)
+        AND label_jagex = 2
         AND pred.Prediction LIKE "Real_player"
 """
 
@@ -22,38 +24,36 @@ sql_get_count_banned_no_data = """
 SELECT
     COUNT(*) as no_data_bans
 FROM Players
-WHERE possible_ban = 1 
+WHERE 1=1
+    AND possible_ban = 1 
     AND confirmed_ban = 0
-    AND (label_jagex = 2 or label_jagex = 3)
+    AND label_jagex = 2
     AND id NOT IN (
         SELECT
-            distinct(Player_id)
+            Player_id
         FROM playerHiscoreDataLatest
     )
 """
 
 sql_get_banned_bots_names = """
     SELECT
-        pls.name as name,
-        pred.Prediction as prediction
-    FROM Players pls
-    JOIN Predictions pred on pred.name = pls.name
-    WHERE possible_ban = 1 
-        AND confirmed_ban = 0
-        AND (label_jagex = 2 or label_jagex = 3)
-        #AND pred.Prediction NOT LIKE "Real_Player"
-        #AND pred.Prediction NOT LIKE "stats_too_low"
-        AND pred.Real_player < 50
+        pl.name as name,
+        pr.Prediction as prediction
+    FROM Players pl
+    JOIN Predictions pr on pred.id = pls.id
+    WHERE 1=1
+        and pl.possible_ban = 1 
+        AND pl.confirmed_ban = 0
+        AND pl.label_jagex = 2
+        AND pr.Real_player < 50
 """
 
 sql_apply_bot_bans = """
-UPDATE Players pls
-JOIN Predictions pred on pred.name = pls.name
-    SET pls.confirmed_ban = 1
+UPDATE Players pl
+JOIN Predictions pr on pr.id = pl.id
+    SET pl.confirmed_ban = 1
 WHERE 1 = 1
-    AND pls.id > 0
-    AND (pls.label_jagex = 2 OR pls.label_jagex = 3)
-    AND pls.possible_ban = 1
-    #AND pred.prediction NOT LIKE "Real_player"
-    AND pred.Real_player < 50
+    AND pl.label_jagex = 2
+    AND pl.possible_ban = 1
+    AND pr.Real_player < 50
 """
